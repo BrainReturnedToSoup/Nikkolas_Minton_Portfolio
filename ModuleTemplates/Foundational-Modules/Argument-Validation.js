1c8a0216b7bfd935abc639304004676ea2d4c729
Nikkolas <minton.nikkolas@gmail.com>
2023-08-25 22:47:51 -0500

    added a new foundational module that will validate arguments baased on a supplied argument validation rule set you supply to the class instance constructor

---
+++ ModuleTemplates/Foundational-Modules/Argument-Validation.js
@@ -0,0 +1,64 @@
+export class ArgumentValidation {
+  constructor(validationRules) {
+    this.#validationRules = validationRules;
+  }
+
+  //--------------STATE-AND-CONFIG-DATA---------------//
+
+  #validationRules = {};
+
+  //------------------HELPER-METHODS------------------//
+
+  #validate = {
+    type: (suppliedArg, argName, methodOrigin, correctType) => {
+      if (typeof suppliedArg !== correctType) {
+        throw new Error(
+          `Argument '${argName}' for method '${methodOrigin}' failed type validation,
+               received '${suppliedArg}' which has a type of '${typeof suppliedArg}',
+                needs to have the type '${correctType}'`
+        );
+      }
+    },
+    instanceof: (suppliedArg, argName, methodOrigin, correctInstance) => {
+      if (!(suppliedArg instanceof correctInstance)) {
+        throw new Error(
+          `Argument '${argName}' for method '${methodOrigin}' failed instance validation,
+               received '${suppliedArg}' which is not an instance of '${correctInstance}'`
+        );
+      }
+    },
+  };
+
+  #argValidator(methodName, argsObj) {
+    if (this.#validationRules.hasOwnProperty(methodName)) {
+      const methodValidationData = this.#validationRules[methodName];
+
+      for (let arg in argsObj) {
+        const argValue = argsObj[arg];
+
+        //check if a supplied arg is a valid arg to supply
+        if (!methodValidationData.hasOwnProperty(arg)) {
+          throw new ReferenceError(
+            `Unrecognized argument for a specific method, received '${arg}' with a value of '${argsObj[arg]}'`
+          );
+        }
+
+        //go down the list of properties to check for on the specific arg
+        for (let property in methodValidationData[arg]) {
+          const correctValue = methodValidationData[arg][property]; //retrieve the data that will be used as a reference for validating the arg
+
+          this.#validate[property](argValue, arg, methodName, correctValue); //validate the arg based on the property being checked currently
+        }
+      }
+    } else {
+      throw new ReferenceError(
+        `Failed to validate the supplied arguments for a specific method, validation data
+             corresponding to this method does not exist, received '${methodName}' as the method being validated`
+      );
+    }
+  }
+
+  validate(methodName, argsObj) {
+    this.#argValidator(methodName, argsObj);
+  }
+}
